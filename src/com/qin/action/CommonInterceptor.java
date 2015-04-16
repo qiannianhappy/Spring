9b0d7110c0ce217e0aa3b114d02d8d7d7df6c94a
qiannianhappy <qinjian4513529@126.com>
2015-04-16 17:33:54 +0800

    拦截器完善

--- src/com/qin/action/CommonInterceptor.java
+++ src/com/qin/action/CommonInterceptor.java
@@ -1,28 +1,33 @@
 package com.qin.action;
 
+import java.lang.reflect.Method;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import com.qin.entity.User;
 
 public class CommonInterceptor extends HandlerInterceptorAdapter  {
-
+	
+	public final Log log = LogFactory.getLog(getClass());
 	@Override
 	public boolean preHandle(HttpServletRequest request,
-			HttpServletResponse response, Object handler) throws Exception {
+		HttpServletResponse response, Object handler) throws Exception {
 		// TODO Auto-generated method stub
+		long startTime=System.currentTimeMillis();
+		request.setAttribute("startTime", startTime);
 		
-		 String className = handler.getClass().getName();// packageName.ClassName   
-		 User user = (User) request.getSession().getAttribute("LOGIN_USER");
+		//Method Method = handler.getClass().getDeclaredMethod(name, null);
+		String url=request.getRequestURL().toString(); 
+		User user = (User) request.getSession().getAttribute("LOGIN_USER");
 			if (user == null) {
-				if (className.equals("home")){
-					//return "login"; // 跳转到管理后台登录页
-				}else{
-					//return "timeout"; // 跳转到弹出窗口登录页
-			}
+				response.sendRedirect(request.getContextPath()+"/login.action");
+				return false; // 跳转到管理后台登录页
 		}
 		return true;  
 	}
@@ -31,7 +36,11 @@ public class CommonInterceptor extends HandlerInterceptorAdapter  {
 			HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
 		// TODO Auto-generated method stub
-		
+		String url=request.getRequestURL().toString();    
+		long startTime=(Long)request.getAttribute("startTime");
+		request.removeAttribute("startTime");
+		long endTime=System.currentTimeMillis();
+		log.info(url+" 操作耗时 "+(endTime-startTime)+" ms");
 	}
 
 	@Override
